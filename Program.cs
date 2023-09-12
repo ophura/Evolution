f9da52a057148adee3a1b6bccfcadb4099f5c138
Ophura <61603742+ophura@users.noreply.github.com>
2023-09-12 18:01:23 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Uselessness;
+
+static void Print<T>(IEnumerable<T> collection)
+{
+    var count = collection.Count() - 2;
+    var index = -1;
+
+    foreach (var item in collection)
+    {
+        Console.Write(item);
+        
+        if (index++ < count) Console.Write(", ");
+    }
+
+    Console.WriteLine('\n');
+}
+
+var array = new int[50]
+{
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+    31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50
+};
+
+array = Evolution.GetOddIndexedElements(array);
+
+Print(array);
+
+var bArray = new byte[50]
+{
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+    31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50
+};
+
+bArray = Evolution.GetOddIndexedElements(bArray);
+
+Print(bArray);
+
+var list = new List<int>(50)
+{
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+    31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50
+};
+
+list = Evolution.GetOddIndexedElements(list);
+
+Print(list);
+
+var bList = new List<byte>(50)
+{
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+    31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50
+};
+
+bList = Evolution.GetOddIndexedElements(bList);
+
+Print(bList);
+
+IList<int> iList = new int[50]
+{
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+    31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50
+};
+
+iList = Evolution.GetOddIndexedElements(iList);
+
+Print(iList);
+
+IReadOnlyList<int> iReadOnlyList = new int[50]
+{
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+    31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50
+};
+
+iReadOnlyList = Evolution.GetOddIndexedElements(iReadOnlyList);
+
+Print(iReadOnlyList);
+
+IEnumerable<int> iEnumerable = new int[50]
+{
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+    31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50
+};
+
+iEnumerable = Evolution.GetOddIndexedElements(iEnumerable);
+
+Print(iEnumerable);
+
+IEnumerable<byte> bIEnumerable = new byte[50]
+{
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+    31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50
+};
+
+bIEnumerable = Evolution.GetOddIndexedElements(bIEnumerable);
+
+Print(bIEnumerable);
