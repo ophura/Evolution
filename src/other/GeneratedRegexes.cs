9e954211ef5a5801d53076c648c41f2d84e15f87
Ophura <61603742+ophura@users.noreply.github.com>
2023-11-20 00:09:53 +0100

    Create GeneratedRegexes.cs

---
+++ src/other/GeneratedRegexes.cs
@@ -0,0 +1,9 @@
+namespace WinDbg;
+
+using System.Text.RegularExpressions;
+
+internal partial class GeneratedRegexes
+{
+    [GeneratedRegex(@"\+0x([0-9a-zA-Z]{3,})\s*(\w+)\s*\:\s*(\[?[0-9]*\]?)\s*(\w+\s?\w+)")]
+    internal static partial Regex DumpPattern();
+}
